dabf1d100dc7c05c09edd04660291e37d3825d7d
Adam <adamplesca1@gmail.com>
2024-08-28 14:32:45 +0100

    old files, will refactor

---
+++ index.js
@@ -0,0 +1,4 @@
+//add lives, once lives are dead game ends 
+//track total score of player
+//generate random math questions, 50/50 
+//add time limit per question, so player does the math mentally while also
